8d260e11818a5b1dbd8e5d755b8a439857ad0168
Gavin Mogan <git@gavinmogan.com>
2021-04-21 19:16:50 -0700

    more bootstrapy, so i can have a sidebar with types

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,46 +4,80 @@ import {graphql, Link} from 'gatsby';
 import {GatsbyImage} from 'gatsby-plugin-image';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320
-};
-
-// markup
 const IndexPage = ({
   data: {
-    allRecipesYaml: {recipies}
+    allRecipesYaml: {
+      recipies,
+      allCuisines,
+      allCourses
+    }
   }
 }) => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Recipes
-      </h1>
-      <div className="row">
-        {recipies.map(({recipe}) => {
-          return (
-            <div key={recipe.id} className="col-xs-12 col-sm-6 col-md-3">
-              <div className="card w-100 h-100">
-                {recipe.image ? <GatsbyImage className="card-img-top" image={recipe.image.childImageSharp.gatsbyImageData} alt={recipe.title} /> : <div>FIXME</div>}
-                <div className="card-body">
-                  <h5 className="card-title"><Link to={recipe.fields.slug}>{recipe.title}</Link></h5>
-                </div>
-              </div>
+    <>
+      <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <Link className="navbar-brand col-sm-3 col-md-2 mr-0" to="/">Recipes</Link>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+            <div className="sidebar-sticky">
+              <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                <span>Courses</span>
+              </h6>
+              <ul className="nav flex-column mb-2">
+                {allCourses.map(c => {
+                  return (
+                    <li key={c.fieldValue} className="nav-item">
+                      <Link className="nav-link" to="/">
+                        {c.fieldValue}
+                        {' '}
+                        <span className="badge  badge-pill badge-secondary">{c.totalCount}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+
+              <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                <span>Cuisines</span>
+              </h6>
+              <ul className="nav flex-column mb-2">
+                {allCuisines.map(c => {
+                  return (
+                    <li key={c.fieldValue} className="nav-item">
+                      <Link className="nav-link" to="/">
+                        {c.fieldValue}
+                        {' '}
+                        <span className="badge badge-secondary">{c.totalCount}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-          );
-        })}
+          </nav>
+
+          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+            <div className="row">
+              {recipies.map(({recipe}) => {
+                return (
+                  <div key={recipe.id} className="col-xs-12 col-sm-6 col-md-3">
+                    <div className="card w-100 h-100">
+                      {recipe.image ? <GatsbyImage className="card-img-top" image={recipe.image.childImageSharp.gatsbyImageData} alt={recipe.title} /> : <div>FIXME</div>}
+                      <div className="card-body">
+                        <h5 className="card-title"><Link to={recipe.fields.slug}>{recipe.title}</Link></h5>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            {/* <pre>{JSON.stringify(recipies, null, 2)}</pre> */}
+          </main>
+        </div>
       </div>
-      {/* <pre>{JSON.stringify(recipies, null, 2)}</pre> */}
-    </main>
+    </>
   );
 };
 
@@ -52,6 +86,14 @@ export default IndexPage;
 export const pageQuery = graphql`
   query {
     allRecipesYaml(sort: { order: DESC, fields: [title] }) {
+      allCuisines: group(field: cuisine) {
+        totalCount
+        fieldValue
+      }
+      allCourses: group(field: course) {
+        totalCount
+        fieldValue
+      }
       recipies: edges {
         recipe: node {
           id
